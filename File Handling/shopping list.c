ea5f06db3ae2c51bc93ae006482c663323226e5e
zainab7804 <123864934+zainab7804@users.noreply.github.com>
2025-03-08 11:06:39 +0530

    Update shopping list.c

--- File Handling/shopping list.c
+++ File Handling/shopping list.c
@@ -1,26 +1,26 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int main()
-//{
-//    int i;
-//    char a[50];
-//    FILE *file=fopen("example.txt","w");
-//
-//    for(i=0;i<10;i++)
-//    {
-//
-//        printf("Enter the list items:");
-//        scanf("%s",&a);
-//        fprintf(file, "%s\n",&a);
-//    }
-//    if (file==NULL)
-//    {
-//        printf("Error opening file for writing!\n");
-//        return 1;
-//    }
-//
-//    fclose(file);
-//    printf("Data written to file Successfully!");
-//    return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    char a[50];
+    FILE *file=fopen("example.txt","w");
+
+    for(i=0;i<10;i++)
+    {
+
+        printf("Enter the list items:");
+        scanf("%s",&a);
+        fprintf(file, "%s\n",&a);
+    }
+    if (file==NULL)
+    {
+        printf("Error opening file for writing!\n");
+        return 1;
+    }
+
+    fclose(file);
+    printf("Data written to file Successfully!");
+    return 0;
+}
